be39f21bfa463242deed153f919307c9588022f5
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-06-23 14:20:44 +0530

    Update imgeditor.py

--- SaitamaRobot/modules/imgeditor.py
+++ SaitamaRobot/modules/imgeditor.py
@@ -452,10 +452,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
 __mod_name__ = "Image Editor"
 __help__ = """
- *IMAGE EDITOR*
 Esdeath has some advanced image editing tools inbuilt
-Bright, Circle, RemBG, Blur, Border, Flip, Glitch, Sticker maker and more
+Bright, Mixed, Circle, Remove BG, Blur, Border, Invert, Glitch, Sticker Maker, Black And White, Rotate, Sapia, Contrast, Pencil And Cartoon.
 
-- /edit [reply to image]: Open the image editor
-- /rmbg [REPLY]: Revove BG of replied image/sticker.
+- /edit [reply to image]: Open the image editor.
 """

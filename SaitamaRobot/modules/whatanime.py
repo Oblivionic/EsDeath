11a65d7ff30f7ffd2f9e4ecc8e74e448be9fe57f
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-08-23 18:04:33 +0530

    Update whatanime.py

--- SaitamaRobot/modules/whatanime.py
+++ SaitamaRobot/modules/whatanime.py
@@ -1,4 +1,3 @@
-
 import os
 import time
 import html
@@ -46,7 +45,7 @@ def calculate_eta(current, total, start_time):
     return ', '.join(thing)
 
 
-@pbot.on_message(filters.command('whatanime'))
+@pbot.on_message(filters.command("whatanime", prefixes=(["!", "/"])))
 async def whatanime(c: Client, m: Message):
     media = m.photo or m.animation or m.video or m.document
     if not media:
@@ -54,58 +53,60 @@ async def whatanime(c: Client, m: Message):
         if not getattr(reply, 'empty', True):
             media = reply.photo or reply.animation or reply.video or reply.document
     if not media:
-        await m.reply_text('Photo or GIF or Video required')
+        await m.reply_text('Please reply it to a Photo or Gif or Video to work')
         return
     with tempfile.TemporaryDirectory() as tempdir:
-        reply = await m.reply_text('Downloading...')
+        reply = await m.reply_text('Downloading media...')
         path = await c.download_media(media, file_name=os.path.join(tempdir, '0'), progress=progress_callback, progress_args=(reply,))
         new_path = os.path.join(tempdir, '1.png')
         proc = await asyncio.create_subprocess_exec('ffmpeg', '-i', path, '-frames:v', '1', new_path)
         await proc.communicate()
-        await reply.edit_text('Uploading...')
+        await reply.edit_text('Uploading media to trace.moe...')
         with open(new_path, 'rb') as file:
-            async with session.post('https://trace.moe/api/search', data={'image': file}) as resp:
+            async with session.post('https://api.trace.moe/search?anilistInfo', data={"image": file}) as resp:
                 json = await resp.json()
     if isinstance(json, str):
         await reply.edit_text(html.escape(json))
     else:
         try:
-            match = next(iter(json['docs']))
+            match = json.get("result")
         except StopIteration:
-            await reply.edit_text('No match')
+            await reply.edit_text("No match")
         else:
-            nsfw = match['is_adult']
-            title_native = match['title_native']
-            title_english = match['title_english']
-            title_romaji = match['title_romaji']
-            synonyms = ', '.join(match['synonyms'])
-            filename = match['filename']
-            tokenthumb = match['tokenthumb']
-            anilist_id = match['anilist_id']
-            episode = match['episode']
-            similarity = match['similarity']
-            from_time = str(datetime.timedelta(seconds=match['from'])).split(
+            match = match[0]
+            title_native = match["anilist"]["title"]["native"]
+            title_english = match["anilist"]["title"]["english"]
+            title_romaji = match["anilist"]["title"]["romaji"]            
+            anilist_id = match["anilist"]["id"]
+            episode = match["episode"]
+            similarity = match["similarity"]
+            synonyms = match["anilist"]["synonyms"]
+            is_adult = match["anilist"]["isAdult"]
+            from_time = str(datetime.timedelta(seconds=match["from"])).split(
                 '.', 1)[0].rjust(8, '0')
-            to_time = str(datetime.timedelta(seconds=match['to'])).split(
-                '.', 1)[0].rjust(8, '0')
-            at_time = match['at']
-            text = f'<a href="https://anilist.co/anime/{anilist_id}">{title_romaji}</a>'
+            to_time = str(datetime.timedelta(seconds=match["to"])).split(
+                '.', 1)[0].rjust(8, '0')   
+            text = f"**WhatAnime Search Results**"
+            if title_romaji:
+                text += f"\n`{title_romaji}`"
             if title_english:
-                text += f' ({title_english})'
+                text += f"\n`{title_english}`"
             if title_native:
-                text += f' ({title_native})'
+                text += f"\n`{title_native}`"
             if synonyms:
-                text += f'\n<b>Synonyms:</b> {synonyms}'
-            text += f'\n<b>Similarity:</b> {(Decimal(similarity) * 100).quantize(Decimal(".01"))}%\n'
+                text += f"\n<b>Synonyms:</b> {synonyms}"
+            if is_adult:
+                text += f"\n<b>NSFW:</b> True"
+            
+            text += f"\n<b>Similarity:</b> {(Decimal(similarity) * 100).quantize(Decimal(".01"))}%"
             if episode:
-                text += f'<b>Episode:</b> {episode}\n'
-            if nsfw:
-                text += '<b>Hentai/NSFW:</b> no'
+                text += f"\n<b>Episode:</b> {episode}"
+
+            text += f"\n<a href='https://anilist.co/anime/{anilist_id}'>View In Anilist</a>"
 
             async def _send_preview():
-                url = f'https://media.trace.moe/video/{anilist_id}/{urlencode(filename)}?t={at_time}&token={tokenthumb}'
                 with tempfile.NamedTemporaryFile() as file:
-                    async with session.get(url) as resp:
+                    async with session.get(match["video"]) as resp:
                         while True:
                             chunk = await resp.content.read(10)
                             if not chunk:
@@ -136,7 +137,6 @@ async def progress_callback(current, total, reply):
             download_speed = '0 B'
         text = f'''Downloading...
 <code>{return_progress_string(current, total)}</code>
-
 <b>Total Size:</b> {format_bytes(total)}
 <b>Downladed Size:</b> {format_bytes(current)}
 <b>Download Speed:</b> {download_speed}/s
